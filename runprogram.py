9fc0f938b694acf8d41802ad970b6840c56ecae8
Prazol Malla <prajwalmalla16@gmail.com>
2023-11-10 14:38:47 +0545

    minor changes

--- runprogram.py
+++ runprogram.py
@@ -1,9 +1,26 @@
-from subprocess import call 
+import os
+import sys
+from subprocess import call
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+def open_script(script_name):
+    script_path = os.path.join(current_directory, script_name)
+    try:
+        call([sys.executable, script_path])
+    except Exception as e:
+        print(f"Error executing {script_name}: {e}")
+
 def open_sign_to_speech():
-    call(['python',"app.py"])
+    open_script("app.py")
 
 def open_speech_to_sign():
-    call(['python','speech-to-text.py'])
+    open_script("speech-to-text.py")
 
 def open_main_ui():
-    call(['python','ui.py'])
+    open_script("ui.py")
+
+
+
+
+
