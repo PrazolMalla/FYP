01ffb10f8e90825aebdaa9dee34d1cdd25b691f9
Prazol Malla <prajwalmalla16@gmail.com>
2023-11-10 08:59:11 +0545

    UI added

--- speech-to-text.py
+++ speech-to-text.py
@@ -1,9 +1,29 @@
-import speech_recognition 
+import speech_recognition as sl
 import os
 import cv2
+from customtkinter import *
+from runprogram import open_main_ui
+from PIL import Image,ImageTk
+import time
+
+main_bg_color ="#CEC6A1"
+frame_bg_color = "#ddd8bf"
+button_bg_color = "#D7263D"
+white_color = "#ffffff"
+black_color = "#111111"
+brown_color = "#3c2410"
+logo_path = "images/logo.png"
+mic_path = "images/mic.png"
+exit_path = "images/exit1.png"
+
+def exit ():
+    root.destroy()
+    open_main_ui()
+
 def play_video(word):
-        # Check if the video file exists for the given word
+    # Check if the video file exists for the given word
     video_file_path = f"video/{word}.mp4"
+    
     if not os.path.exists(video_file_path):
         cap = cv2.VideoCapture("video/idle.mp4")
     else:
@@ -15,39 +35,93 @@ def play_video(word):
         ret, frame = cap.read()
         if not ret:
             break
-        frame = cv2.resize(frame,(540,960))
-        cv2.imshow('Video', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        frame = cv2.resize(frame,(308,548))
+        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        photo = ImageTk.PhotoImage(Image.fromarray(frame))
+        canvas.create_image(0,0,image=photo,anchor = NW)
+        canvas.image = photo
+        canvas.update()
+
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
+
 
     cap.release()
-    
 
-sr = speech_recognition.Recognizer()
+def speech():  
+        
+        label2.configure(text="")
+        label2.update()
+        sr = sl.Recognizer()
+        with sl.Microphone() as source2:
+            btn1.configure(text="Silent Please")
+            btn1.update()
+            sr.adjust_for_ambient_noise(source2,duration=2)
 
-with speech_recognition.Microphone() as source2:
-    print("Silent Please")
+            btn1.configure(text="Speak Now Please")
+            btn1.update()
+            audio2 = sr.listen(source2)
+            try:
+                text = sr.recognize_google(audio2)
+                text = text.lower()
+                label1.configure(text= f"You said: {text.upper()} ",fg_color=button_bg_color,padx=4,pady=4)
+                label1.update()
+                keywords = list()
+                keywords = text.split()
+                for keyword in keywords:
+                    play_video(keyword)
+                canvas.image = None 
+                canvas.update()
+                label1.configure(text="",fg_color="transparent")
+                label1.update()  
+            except sl.UnknownValueError:
+                print("Speech Recognition could not understand audio")
+                label2.configure(text="Sorry, Couldn't Understand You")
+                label2.update()
+                # time.sleep(2)
+            except sl.RequestError as e:
+                print(f"Could not request results from Google Speech Recognition service; {e}")
+                label2.configure(text="Speech Recognition error")
+                label2.update()
+            finally:
+                btn1.configure(text="Speech to Sign")
+                btn1.update()
+             
 
-    sr.adjust_for_ambient_noise(source2,duration=2)
+root = CTk()
+root.title("Speech to Sign")
+root.configure(fg_color =main_bg_color)
+root.geometry("1920x1080")
+root.after(0,root.wm_state,"zoomed")
+frame = CTkFrame(master=root,width=600,height=960,fg_color = frame_bg_color)
+frame.place(relx = 0.5,rely = 0.5,anchor="center")
 
-    print("Speak now Please")
+canvas = CTkCanvas(frame,width=310,height=550,bg=frame_bg_color,borderwidth=0,highlightthickness=0)
+canvas.place(relx=0.3,rely=0.3)
+logo_image = CTkImage(light_image=Image.open(logo_path),
+                                  dark_image=Image.open(logo_path),
+                                  size=(200, 75))
+image_label = CTkLabel(frame,image=logo_image,text="")
+image_label.place(relx = 0.5,rely = 0.15,anchor = "center")
+label3 =CTkLabel(master= frame,text="SPEECH TO SIGN TRANSLATION",text_color=brown_color,font=("Arial",-24))
+label3.place(relx = 0.5 , rely=0.2,anchor = "center")
+label1 = CTkLabel(master=frame,text="",text_color=white_color,font=("Arial",-18))
+label1.place(relx = 0.5 , rely=0.8,anchor = "center")
+label2 = CTkLabel(master=frame,text="",text_color=button_bg_color,font=("Arial",-14))
+label2.place(relx = 0.5 , rely=0.285,anchor = "center")
 
-    audio2 = sr.listen(source2)
-
-    texxt = sr.recognize_google(audio2)
-
-    texxt = texxt.lower()
-
-    print("Did you Say:_ "+texxt)
-
-keywords = list()
-
-keywords = texxt.split()
-print(keywords)
-
-for keyword in keywords:
-    play_video(keyword)    
-
-cv2.destroyAllWindows()
-
-
+mic_image = CTkImage(light_image=Image.open(mic_path),
+                                  dark_image=Image.open(mic_path),
+                                  size=(30, 30))
+btn1 =CTkButton(master=frame,text="Sign to Speech",hover_text_color=white_color,corner_radius=20,
+                fg_color=button_bg_color,border_color=button_bg_color,border_width=2,
+                hover_color=button_bg_color,text_color=white_color,width=150,height=40,
+                font=("Arial",-14),image=mic_image,command=speech)
+btn1.place(relx = 0.5 , rely=0.25,anchor = "center")
+exit_image = CTkImage(light_image=Image.open(exit_path),
+                                  dark_image=Image.open(exit_path),
+                                  size=(30, 30))
+exit_btn = CTkButton(master=root,hover_text_color=button_bg_color,text_color=button_bg_color,width=40,height=40,text="",
+                     fg_color="transparent",image=exit_image,hover=False,command=exit)
+exit_btn.place(relx=0.075,rely=0.05,anchor="ne")
+root.mainloop()
--- ui.py
+++ ui.py
@@ -1,24 +1,52 @@
 from customtkinter import *
-from runprogram import open_py_file
+from PIL import Image
+from runprogram import open_sign_to_speech,open_speech_to_sign
+import time
+def exit ():
+    app.destroy()
+
+def speech_to_sign():
+    app.destroy()
+    open_speech_to_sign()
+
+
+main_bg_color ="#CEC6A1"
+frame_bg_color = "#ddd8bf"
+button_bg_color = "#D7263D"
+white_color = "#ffffff"
+black_color = "#111111"
+logo_path = "images/logo.png"
+exit_path = "images/exit.png"
+
 app = CTk()
 app.geometry("1920x1080")
 app.title("SignLink")
-
-app.after(1,app.wm_state,'zoomed')
-# app.attributes("-fullscreen", True)
-# app.bind("<F11>", lambda event: app.attributes("-fullscreen",not app.attributes("-fullscreen")))
-# app.bind("<Escape>", lambda event: app.attributes("-fullscreen", False))
-
-frame1 = CTkFrame(app)
+app.configure(fg_color =main_bg_color)
+app.after(0,app.wm_state,'zoomed')
+exit_image = CTkImage(light_image=Image.open(exit_path),
+                                  dark_image=Image.open(exit_path),
+                                  size=(30, 30))
+exit_btn = CTkButton(master=app,hover_text_color=button_bg_color,text_color=button_bg_color,width=40,height=40,text="",
+                     fg_color="transparent",image=exit_image,hover=False,command=exit)
+exit_btn.place(relx=0.975,rely=0.05,anchor="ne")
+frame1 = CTkFrame(app)    
+frame1.configure(fg_color = frame_bg_color,width = 500,height = 300) 
 frame1.place(relx = 0.5,rely = 0.5,anchor="center")
+logo_image = CTkImage(light_image=Image.open(logo_path),
+                                  dark_image=Image.open(logo_path),
+                                  size=(400, 150))
+image_label = CTkLabel(frame1,image=logo_image,text="")
+image_label.place(relx = 0.5,rely = 0.3,anchor = "center")
+btn1 =CTkButton(master=frame1,text="Sign to Speech",hover_text_color=white_color,corner_radius=20,
+                fg_color='transparent',border_color=button_bg_color,border_width=2,
+                hover_color=button_bg_color,text_color=black_color,width=150,height=40,
+                font=("Arial",-14),command=open_sign_to_speech)
+btn1.place(relx = 0.525,rely = 0.6,anchor = "center" )
 
-btn1 =CTkButton(master=frame1,text="Speech to Sign",text_color="#C6DBF0",corner_radius=20,fg_color='transparent',border_color='#C6DBF0',border_width=2,hover_color='#A0C4E2',hover_text_color="#111111",command=open_py_file)
+btn2 =CTkButton(master=frame1,text="Speech to Sign",hover_text_color=white_color,
+                corner_radius=20,fg_color='transparent',border_color=button_bg_color,
+                border_width=2 ,hover_color=button_bg_color,text_color=black_color,
+                width=150,height=40,font=("Arial",-14),command=speech_to_sign)
+btn2.place(relx = 0.525,rely = 0.75,anchor = "center")
 
-
-btn1.place(relx = 0.5,rely = 0.4,anchor = "center" )
-
-btn2 =CTkButton(master=frame1,text="Sign to Sign",text_color="#C6DBF0",corner_radius=20,fg_color='transparent',border_color='#C6DBF0',border_width=2,hover_color='#A0C4E2',hover_text_color="#111111")
-btn2.place(relx = 0.5,rely = 0.6,anchor = "center" )
-
-# app.attributes('-fullscreen', True)
 app.mainloop()
